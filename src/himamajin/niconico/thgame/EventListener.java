2937a2ced113ecd89a58532a0cc37dde7cf08296
himamajin <ycxdr445@yahoo.co.jp>
2014-07-30 22:33:44 +0900

    TNTのダメージをキャンセルする処理追加

--- src/himamajin/niconico/thgame/EventListener.java
+++ src/himamajin/niconico/thgame/EventListener.java
@@ -1,7 +1,25 @@
 package himamajin.niconico.thgame;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 
 public class EventListener implements Listener {
+	
+	
+	
+	@EventHandler
+	public void onExplosionPrime(ExplosionPrimeEvent event) {	
+	    Entity entity = event.getEntity();
+	 
+	    // このイベントは、点火されたTNTにより発生したのかどうかを確認します。
+	    // （つまり、TNTの爆発はこれで無効化されますが、クリーパーの爆発は無効化されません）
+	    if (entity instanceof TNTPrimed) {
+	        event.setCancelled(true); // イベントをキャンセルして、爆発を無かったことにする
+	        entity.getWorld().createExplosion(entity.getLocation(), 0); // 偽物の爆発を発生させる
+	    }
+	}
 
 }
